3806285e667f5a635bfee7a598e223cecbb427d7
Ezzat Chamudi <chamudi.git@outlook.com>
2019-09-18 00:39:29 +0300

    update sqlite test

--- test/test-sqlite.js
+++ test/test-sqlite.js
@@ -6,15 +6,16 @@
 // npx mocha ./test/test-db.js
 
 const path = process.cwd();
+const assert = require('assert');
 const fs = require('fs');
 const { execSync } = require('child_process');
 
 module.exports.testSqliteExport = function () {
   before('creating db folder', function () {
-    if (fs.existsSync(`${path}/test_temp/db`)) {
-      throw new Error('db folder exists, please delete and rerun the test.');
+    if (fs.existsSync(`${path}/test_temp/cli_sqlite`)) {
+      throw new Error('cli_sqlite folder exists, please delete and rerun the test.');
     } else {
-      fs.mkdirSync(`${path}/test_temp/db`);
+      fs.mkdirSync(`${path}/test_temp/cli_sqlite`);
     }
   });
 
@@ -22,6 +23,8 @@ module.exports.testSqliteExport = function () {
     this.slow(60000);
     this.timeout(300000);
 
-    execSync('jmdict-util sqlite ./test/fixtures/JMdict_e_test -d ./test_temp/db');
+    execSync('jmdict-util sqlite ./test/fixtures/JMdict_e_test -d ./test_temp/cli_sqlite');
+
+    assert.deepStrictEqual(fs.existsSync(`${path}/test_temp/cli_sqlite/jmdict.db`), true);
   });
 };
